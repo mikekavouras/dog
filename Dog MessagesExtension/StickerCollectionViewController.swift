e3f39ec546c18c6b0562fe56ebfe8438115f166b
Mike Kavouras <kavourasm@gmail.com>
2018-10-15 10:25:20 -0400

    Fetch from iCloud

--- Dog MessagesExtension/StickerCollectionViewController.swift
+++ Dog MessagesExtension/StickerCollectionViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Messages
+import CloudKit
 
 private let reuseIdentifier = "Cell"
 
@@ -15,6 +16,19 @@ class StickerCollectionViewController: UICollectionViewController, UICollectionV
     fileprivate let padding: CGFloat = 8
     fileprivate let perRow: CGFloat = 3
     private var stickers = [MSSticker]()
+    
+    lazy private var database: CKDatabase = {
+        let container = CKContainer(identifier: "iCloud.com.mikekavouras.DogCMS")
+        return container.publicCloudDatabase
+    }()
+    
+    lazy private var documentDirectoryPath: String? = {
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        if let documentDirectoryPath = paths.first {
+            return "\(documentDirectoryPath)/Dog/Dog"
+        }
+        return nil
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,38 +38,67 @@ class StickerCollectionViewController: UICollectionViewController, UICollectionV
 
     private func setup() {
         setupStickers()
+        setupFileSystem()
     }
 
     private func setupStickers() {
-        loadStickers()
-    }
-    
-    private func loadStickers() {
-        let dir = "Stickers"
-        let bundle = Bundle(for: type(of: self))
-        stickers = sortedStickerNames.map { name in
-            guard let filename = name.split(separator: ".").first,
-                let url = bundle.url(forResource: "/\(dir)/\(filename)", withExtension: "png") else
-            {
-                return nil
+        guard let path = documentDirectoryPath else { return }
+        
+        fetchStickers { maybeRecords in
+            guard let records = maybeRecords else { return }
+            // generate images from asset data
+            
+            records.forEach { record in
+                if let asset = record["image"] as? CKAsset,
+                    let data = try? Data(contentsOf: asset.fileURL),
+                    let image = UIImage(data: data)
+                {
+                    let filename = "\(path)/\(record.recordID.recordName).png"
+                    print(filename)
+                    let png = UIImagePNGRepresentation(image)
+                    do {
+                        let url = URL(fileURLWithPath: filename)
+                        try png?.write(to: url)
+                        let sticker = try MSSticker(contentsOfFileURL: url, localizedDescription: "")
+                        self.stickers.append(sticker)
+                    }
+                    catch {
+                        print(error)
+                    }
+                }
+                self.collectionView?.reloadData()
             }
-            return try? MSSticker(contentsOfFileURL: url, localizedDescription: "")
-            }.compactMap { $0 }
+        }
     }
     
-    private var sortedStickerNames = [
-        "dog", "cat", "ok", "cute",
-        "woah", "cool", "ha", "wyut",
-        "hai", "hey", "help", "hello",
-        "no", "sorry", "down", "yes",
-        "mine", "call-me", "buds", "sup",
-        "sad", "fish", "ball", "hat",
-        "glasses", "bow", "wow",
-        "nice", "thank-you", "smol",
-        "tell-me-more", "noo", "tricks",
-        "hey2", "this", "aw_baby", "butt",
-        "cold", "never", "what", 
-    ]
+    private func setupFileSystem() {
+        if let path = documentDirectoryPath {
+            let manager = FileManager.default
+            if !manager.fileExists(atPath: path) {
+                do {
+                    try FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+                } catch {
+                    print("Couldn't create your directory")
+                }
+            }
+        }
+    }
+    
+    private func fetchStickers(_ onCompletion: @escaping ([CKRecord]?) -> Void) {
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Sticker", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "sortOrder", ascending: true)]
+        let operation = CKQueryOperation(query: query)
+        operation.qualityOfService = .userInitiated
+        database.perform(query, inZoneWith: nil) { records, error in
+            DispatchQueue.main.async {
+                onCompletion(records)
+            }
+        }
+    }
+}
+
+extension StickerCollectionViewController {
     
     // MARK: UICollectionViewDataSource
 
